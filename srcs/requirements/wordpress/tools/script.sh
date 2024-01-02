f9fd6e412ea42d9821dd60a470e22c63ddee64fc
Elmahdi Amghar <amgharmahdi1@gmail.com>
2024-01-02 21:38:56 +0100

    almost done with  wordpress, thanks to ilyass

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,23 +1,8 @@
 #!/bin/sh
 
-sleep 20
-if [ -f ./wp-config.php ]
-then
-	echo "wordpress already downloaded"
-else
-	#Download wordpress and all config file
-	wget http://wordpress.org/latest.tar.gz
-	tar xfz latest.tar.gz
-	mv wordpress/* .
-	rm -rf latest.tar.gz
-	rm -rf wordpress
-
-	#Import env variables in the config file
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$DOMAIN_NAME/g" wp-config-sample.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	cp wp-config-sample.php wp-config.php
-fi
-
-exec "$@"
+wp core download  --allow-root
+wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root
+wp core install --url=$DOMAIN_NAME --title=$DOMAIN_NAME --admin_user=$MYSQL_USER --admin_password=$MYSQL_ROOT_PASSWORD --admin_email=amgharmahdi1@gmail.com --allow-root
+sleep 5
+/usr/sbin/php-fpm7.4 -F
+bash
