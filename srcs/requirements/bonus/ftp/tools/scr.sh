b46a8f0f15424468454b291391a90ca7d0a90408
Elmahdi Amghar <eamghar@e3r10p4.1337.ma>
2024-01-09 19:12:04 +0100

    weee

--- srcs/requirements/bonus/ftp/tools/scr.sh
+++ srcs/requirements/bonus/ftp/tools/scr.sh
@@ -1,17 +1,43 @@
 #!/bin/bash
 
+# mkdir -p /var/run/vsftpd/empty
+# service vsftpd start
+
+# adduser $FTP_USER --disabled-password
+
+# echo "$FTP_USER:$FTP_PASSWORD" | /usr/sbin/chpasswd
+
+# echo "$FTP_USER" | tee -a /etc/vsftpd.userlist 
+
+# chown nobody:nogroup /var/www
+# chmod a-w /var/www
+# chown $FTP_USER:$FTP_USER /var/www
+
+# sed -i -r "s/#write_enable=YES/write_enable=YES/1"   /etc/vsftpd.conf
+# sed -i -r "s/#chroot_local_user=YES/chroot_local_user=YES/1"   /etc/vsftpd.conf
+
+# echo "
+# local_enable=YES
+# allow_writeable_chroot=YES
+# pasv_enable=YES
+# local_root=/var/www
+# pasv_min_port=40000
+# pasv_max_port=40005
+# userlist_file=/etc/vsftpd.userlist" >> /etc/vsftpd.conf
+
+# service vsftpd stop
+
+
+# /usr/sbin/vsftpd
+
+
 mkdir -p /var/run/vsftpd/empty
-service vsftpd start
 
-adduser $FTP_USER --disabled-password
+adduser --home /var/www ${FTP_USER}
 
-echo "$FTP_USER:$FTP_PASSWORD" | /usr/sbin/chpasswd
+echo ${FTP_USER}:${FTP_PASSWORD} | chpasswd
 
-echo "$FTP_USER" | tee -a /etc/vsftpd.userlist 
-
-chown nobody:nogroup /var/www
-chmod a-w /var/www
-chown $FTP_USER:$FTP_USER /var/www
+adduser ${FTP_USER} root
 
 sed -i -r "s/#write_enable=YES/write_enable=YES/1"   /etc/vsftpd.conf
 sed -i -r "s/#chroot_local_user=YES/chroot_local_user=YES/1"   /etc/vsftpd.conf
@@ -28,4 +54,4 @@ userlist_file=/etc/vsftpd.userlist" >> /etc/vsftpd.conf
 service vsftpd stop
 
 
-/usr/sbin/vsftpd
+/usr/sbin/vsftpd /etc/vsftpd.conf
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -6,17 +6,17 @@ wp core install --url=$DOMAIN_NAME --title=$DOMAIN_NAME --admin_user=$MYSQL_ROOT
 wp user create $MYSQL_USER randomMail@gmail.com --role=author --user_pass=$MYSQL_PASSWORD  --allow-root
 
 
+# echo "define('FS_METHOD', 'direct');" >> /var/www/wp-config.php
+# echo "define('FS_CHMOD_DIR', 0775);" >> /var/www/wp-config.php
+# echo "define('FS_CHMOD_FILE', 0664);" >> /var/www/wp-config.php
+
+wp plugin update --all --allow-root
 wp plugin install redis-cache --activate --allow-root
 wp config set WP_REDIS_HOST redis --allow-root
 wp config set WP_REDIS_PORT 6379 --raw --allow-root
 wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
 # wp config set WP_REDIS_PASSWORD $REDIS_PASSWORD --allow-root
 wp config set WP_REDIS_CLIENT phpredis --allow-root
-wp plugin update --all --allow-root
 wp redis enable --allow-root
-
-echo "define('FS_METHOD', 'direct');" >> /var/www/wp-config.php
-echo "define('FS_CHMOD_DIR', 0775);" >> /var/www/wp-config.php
-echo "define('FS_CHMOD_FILE', 0664);" >> /var/www/wp-config.php
 
 /usr/sbin/php-fpm7.4 -F
